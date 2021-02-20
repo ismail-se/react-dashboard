f267f8bbd4e70eb88357c74633157a1fc22e6f06
Muhammad Ismail <ismaileuro16@gmail.com>
2021-02-20 13:14:19 +0500

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Login from "../Login/Login";
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
